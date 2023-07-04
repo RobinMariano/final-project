93d53b503839e8a3af20862e1b5f96b56755e735
RobinMariano <20-04699@g.batstate-u.edu.ph>
2023-07-04 11:30:54 +0800

    new code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/**import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +9,56 @@ export class HomePage {
 
   constructor() {}
 
+} **/
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+export class HomePage {
+  @ViewChild('searchbar', {static: true}) searchbar:  ElementRef | null = null;
+  results: string[] = [];
+  showResults = false;
+  data: string[] = [
+    'Onion', 'Garlic', 'Salt', 'Pepper', 'Oyster Sauce', 'Potato', 'Cabbage', 'Carrots',
+    'Egg', 'Butter'
+  ];
+  selectedIngredients: string[] = [];
+
+  constructor(private elementRef: ElementRef) { }
+  
+
+  handleInput(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.results = this.data.filter((item: string) =>
+      item.toLowerCase().includes(query)
+    );
+    this.showResults = true;
+  }
+
+  selectResult(event: any, result: string) {
+    event.stopPropagation();
+    if (!this.selectedIngredients.includes(result)) {
+      this.selectedIngredients.push(result);
+    }
+    this.showResults = false;
+  }
+
+    deleteIngredient(ingredient: string) {
+      const index = this.selectedIngredients.indexOf(ingredient);
+      if (index > -1) {
+        this.selectedIngredients.splice(index, 1);
+      }
+    }
+
+@HostListener('document:click', ['$event'])
+  handleClick(event: Event) {
+    const isClickedInside = this.searchbar?.nativeElement?.contains(event.target);
+    if (!isClickedInside) {
+      this.showResults = false;
+
+    }
+  }
 }
